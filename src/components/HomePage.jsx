49d9077b0bd2c272f385a86bac3058b1eefeef46
Ahmed Hussain <ahmedhussain@Ahmeds-MacBook-Pro.local>
2024-03-11 14:37:25 +0000

    created and styled a functional page to display all articles

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage(){
+
+    return (
+       <div className="Page">
+       <h2>Welcome to the NorthCoders News Network </h2>
+       
+       </div> 
+    )
+}
+export default HomePage
