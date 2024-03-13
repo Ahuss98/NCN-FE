f49485d37f15aefa6101a7930db8ab6282630618
Ahmed Hussain <ahmedhussain@Ahmeds-MacBook-Pro.local>
2024-03-13 10:06:47 +0000

    added some baisic error handling for required fields and more

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React from 'react';
 
-function HomePage(){
-
-    return (
-       <div className="Page">
-       <h2>Welcome to the NorthCoders News Network </h2>
-       
-       </div> 
-    )
+function HomePage() {
+	return (
+		<div className="Page">
+			<h2>Welcome to the NorthCoders News Network </h2>
+		</div>
+	);
 }
-export default HomePage
+export default HomePage;
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,92 +1,103 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "https://ncn-network.onrender.com/api/articles/";
+const BASE_URL = 'https://ncn-network.onrender.com/api/articles/';
 
-export const fetchArticles = ( () => {
-    return axios.get(`${BASE_URL}`)
-    .then((response) => {
-        return response.data
-    })
-    .catch((error) => {
-        console.error("Error fetching articles:", error);
-        throw error;
-      });
-})
+export const fetchArticles = () => {
+	return axios
+		.get(`${BASE_URL}`)
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			console.error('Error fetching articles:', error);
+			throw error;
+		});
+};
 
 export const fetchArticleByTopic = (topic) => {
-    return axios.get(`https://ncn-network.onrender.com/api/articles?topic=${topic}`)
-    .then((response) => {
-        return response.data
-    })
-}
+	return axios
+		.get(`https://ncn-network.onrender.com/api/articles?topic=${topic}`)
+		.then((response) => {
+			return response.data;
+		});
+};
 
-export const fetchSortedArticle = (topic, selectedSort , order ) => {
-    return axios.get(`https://ncn-network.onrender.com/api/articles?topic=${topic}&sort_by=${selectedSort}&order=${order}`)
-    .then((response) => {
-        return response.data
-    })
-}
+export const fetchSortedArticle = (topic, selectedSort, order) => {
+	return axios
+		.get(
+			`https://ncn-network.onrender.com/api/articles?topic=${topic}&sort_by=${selectedSort}&order=${order}`
+		)
+		.then((response) => {
+			return response.data;
+		});
+};
 
 export const fetchTopics = () => {
-    return axios.get('https://ncn-network.onrender.com/api/topics')
-    .then((response) => {
-        return response.data
-    })
-}
+	return axios
+		.get('https://ncn-network.onrender.com/api/topics')
+		.then((response) => {
+			return response.data;
+		});
+};
 
 export const fetchArticle = (articleId) => {
-  return axios.get(`${BASE_URL}${articleId}`)
-    .then((response) => {
-      return response.data
-    })
-    .catch((error) => {
-      console.error("Error fetching article:", error);
-      throw error
-    });
+	return axios
+		.get(`${BASE_URL}${articleId}`)
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			console.error('Error fetching article:', error);
+			throw error;
+		});
 };
 
 export const fetchComments = (articleId) => {
-  return axios.get(`${BASE_URL}${articleId}/comments`)
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      console.error("Error fetching comments:", error)
-      throw error
-    });
+	return axios
+		.get(`${BASE_URL}${articleId}/comments`)
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			console.error('Error fetching comments:', error);
+			throw error;
+		});
 };
 
 export const updateArticleVotes = (articleId, voteChange) => {
-  return axios.patch(`${BASE_URL}${articleId}`, {
-    inc_votes: voteChange,
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      console.error("Error updating article votes:", error);
-      throw error;
-    });
+	return axios
+		.patch(`${BASE_URL}${articleId}`, {
+			inc_votes: voteChange,
+		})
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			console.error('Error updating article votes:', error);
+			throw error;
+		});
 };
 
 export const postComment = (articleId, username, body) => {
-  return axios.post(`${BASE_URL}${articleId}/comments`, {
-    username,
-    body,
-  })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      console.error("Error posting comment:", error);
-      throw error;
-    });
+	return axios
+		.post(`${BASE_URL}${articleId}/comments`, {
+			username,
+			body,
+		})
+		.then((response) => {
+			return response.data;
+		})
+		.catch((error) => {
+			console.error('Error posting comment:', error);
+			throw error;
+		});
 };
 
 export const deleteCommentById = (comment_id) => {
-    return axios.delete(`https://ncn-network.onrender.com/api/comments/${comment_id}`)
-      .catch((error) => {
-        console.error("Error posting comment:", error);
-        throw error;
-      });
-}
+	return axios
+		.delete(`https://ncn-network.onrender.com/api/comments/${comment_id}`)
+		.catch((error) => {
+			console.error('Error posting comment:', error);
+			throw error;
+		});
+};
